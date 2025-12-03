7150c629de888a9a39632ca7a3cfe8493626c397
Abdulkaium05 <abdulkaiumiqbel2005@gmail.com>
2025-12-02 20:07:04 -0800

    new files and folders

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,4 +1,8 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> f78bb2b03b5eccb8995035f732f7e7586f549c8f
+export const firebaseConfig = {
+  "projectId": "echob-87513860-76a14",
+  "appId": "1:584611986272:web:32e120f23534b5d58b7c05",
+  "apiKey": "AIzaSyDCzNKo-F2BJGgk0qgvo0kp2Bst-IiXWVI",
+  "authDomain": "echob-87513860-76a14.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "584611986272"
+};
